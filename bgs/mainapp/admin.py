f54e63c935ad751f99cb8f8d01d2b85f1a4bf57f
jhzII <jhzll@yandex.ru>
2021-11-18 00:47:51 +0300

    Корректировка моделей, Статические файлы, Админка.

--- bgs/mainapp/admin.py
+++ bgs/mainapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Game, Match, MemberList
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Game)
+admin.site.register(Match)
+admin.site.register(MemberList)
--- bgs/mainapp/models.py
+++ bgs/mainapp/models.py
@@ -2,21 +2,22 @@ from django.db import models
 
 
 class User(models.Model):
-    username = models.CharField(max_length=50, verbose_name='Имя пользователя')
-    login = models.EmailField(verbose_name='Email')
+    username = models.CharField(max_length=50, unique=True, verbose_name='Имя пользователя')
+    login = models.EmailField(unique=True, verbose_name='Email')
     fname = models.CharField(max_length=50, verbose_name='Имя')
     lname = models.CharField(max_length=50, verbose_name='Фамилия')
     date_of_birth = models.DateField(blank=True, verbose_name='Дата рождения')
-    image = models.ImageField(verbose_name='Изображение')
+    image = models.ImageField(blank=True, verbose_name='Изображение')
     # todo: whitelist
     # todo: blacklist
 
 
 class Game(models.Model):
     name = models.CharField(max_length=50, verbose_name='Название')
+    slug = models.SlugField(unique=True)
     description = models.TextField(verbose_name='Описание')
     players = models.CharField(max_length=255, verbose_name='Количество игроков')
-    rules = models.TextField(verbose_name='Пользовательские правила')
+    rules = models.TextField(blank=True, verbose_name='Пользовательские правила')
     image = models.ImageField(verbose_name='Изображение')
     # todo: rating
 
@@ -30,4 +31,4 @@ class Match(models.Model):
 class MemberList(models.Model):
     match = models.ForeignKey(Match, on_delete=models.CASCADE, verbose_name='Матч')
     member = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='Участник')
-    place = models.PositiveSmallIntegerField(default=0, verbose_name='Место')
+    place = models.PositiveSmallIntegerField(verbose_name='Место')
